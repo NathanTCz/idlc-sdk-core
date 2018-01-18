b3fa52f28c8ef56840dc97d3abb2bf5c230900a7
Nathan Cazell <nathan.cazell@imageapi.com>
2018-01-18 12:58:55 -0500

    move service definitions to core

--- lib/idlc-sdk-core.rb
+++ lib/idlc-sdk-core.rb
@@ -7,6 +7,22 @@ require 'idlc-sdk-core/utility'
 require 'idlc-sdk-core/workspace'
 require 'idlc-sdk-core/zip'
 
+# Service Definitions
+$services = {
+  'config' => {
+    'endpoint' => 'un0t03st4m.execute-api.us-east-1.amazonaws.com',
+    'stage' => 'dev'
+  },
+  'deploy' => {
+    'endpoint' => 'dwervfhpxe.execute-api.us-east-1.amazonaws.com',
+    'stage' => 'dev'
+  },
+  'status' => {
+    'endpoint' => 'xmztqnqkb8.execute-api.us-east-1.amazonaws.com',
+    'stage' => 'dev'
+  }
+}
+
 module Idlc
   # ...
 end
--- lib/idlc-sdk-core/version.rb
+++ lib/idlc-sdk-core/version.rb
@@ -1,3 +1,3 @@
 module Idlc
-  VERSION = '1.0.0.rc3'.freeze
+  VERSION = '1.0.0.rc4'.freeze
 end
