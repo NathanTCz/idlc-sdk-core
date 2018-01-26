9ab5c82aa51b72875c5299767b121064a377410c
Nathan Cazell <nathan.cazell@imageapi.com>
2018-01-26 09:22:33 -0500

    change service definitions, move restclient to core lib

--- lib/idlc-sdk-core.rb
+++ lib/idlc-sdk-core.rb
@@ -6,23 +6,26 @@ require 'idlc-sdk-core/helpers'
 require 'idlc-sdk-core/utility'
 require 'idlc-sdk-core/workspace'
 require 'idlc-sdk-core/zip'
-
-# Service Definitions
-$services = {
-  'config' => {
-    'endpoint' => 'un0t03st4m.execute-api.us-east-1.amazonaws.com',
-    'stage' => 'dev'
-  },
-  'deploy' => {
-    'endpoint' => 'dwervfhpxe.execute-api.us-east-1.amazonaws.com',
-    'stage' => 'dev'
-  },
-  'status' => {
-    'endpoint' => 'xmztqnqkb8.execute-api.us-east-1.amazonaws.com',
-    'stage' => 'dev'
-  }
-}
+require 'idlc-sdk-core/restclient'
 
 module Idlc
-  # ...
+  # Service Definitions
+  SERVICES = {
+    'build' => {
+      'endpoint' => 'build.orchestrate.imageapi.com',
+      'stage' => 'dev'
+    },
+    'config' => {
+      'endpoint' => 'config.orchestrate.imageapi.com',
+      'stage' => 'dev'
+    },
+    'deploy' => {
+      'endpoint' => 'deploy.orchestrate.imageapi.com',
+      'stage' => 'dev'
+    },
+    'status' => {
+      'endpoint' => 'status.orchestrate.imageapi.com',
+      'stage' => 'dev'
+    }
+  }.freeze
 end
